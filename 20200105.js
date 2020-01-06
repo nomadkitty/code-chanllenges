e2a95e88125e2a745280b9173de04e64a308e547
Jojo Zhang <jojozhang0327@gmail.com>
2020-01-06 02:48:52 +0000

    amplify

---
+++ 20200105.js
@@ -0,0 +1,14 @@
+function amplify(num) {
+  return Array(num).fill(1)
+    .map((v,i) => v + i)
+    .map(v => v % 4 == 0 ? v * 10 : v)
+
+}
+
+function amplify2(num) {
+    let arr = [];
+    for (let i=1; i <= num; i++){
+        arr.push(i)
+    }
+    return arr.map(item => item % 4 === 0 ? item*10 : item)
+}
