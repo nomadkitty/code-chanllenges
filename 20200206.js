2b627aa242b0ae937e26a6fa050823c4add2d33a
Jojo Zhang <jojozhang0327@gmail.com>
2020-02-06 23:38:07 +0000

    2 more challenges

--- 20200206.js
+++ 20200206.js
@@ -2,3 +2,11 @@ function howManyTimes(num) {
 	let str = "a";
 	return `Ed${str.repeat(num)}bit`
 }
+
+function noOdds(arr) {
+	return arr.filter(ele=> ele%2 ===0)
+}
+
+function removeNull(arr) {
+	return arr.filter(ele => ele !== null)
+}
