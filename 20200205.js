f4dfdb37f19f11a1ef4315b6d91c503e7204a895
Jojo Zhang <jojozhang0327@gmail.com>
2020-02-06 04:00:47 +0000

    toy car factory

--- 20200205.js
+++ 20200205.js
@@ -3,3 +3,7 @@ function canCapture([yourRook, opponentsRook]) {
 	const arr2 = [...opponentsRook];
 	return (arr1[0] === arr2[0] || arr1[1] ===arr2[1]) ? true:false;
 }
+
+function cars(wheels, bodies, figures) {
+	return wheels < 4 ? 0:Math.min(Math.floor(wheels/4), bodies, Math.floor(figures/2))
+}
