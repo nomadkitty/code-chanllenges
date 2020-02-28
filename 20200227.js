08680e35decfc0389830d2613d1e8f708ed82914
Jojo Zhang <jojozhang0327@gmail.com>
2020-02-27 23:16:49 -0500

    Add removeVowels and hurdleJump Edabit challenge

--- 20200227.js
+++ 20200227.js
@@ -1,3 +1,12 @@
 function greetPeople(names) {
 	return names.map(name => `Hello ${name}`).join(", ")
 }
+
+// Fix this incorrect code, so that all tests pass!
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '')
+}
+
+function hurdleJump(hurdles, jumpHeight) {
+	return jumpHeight >= Math.max(...hurdles)
+}
