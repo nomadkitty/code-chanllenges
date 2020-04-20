5b44bdb0f2b59ecdb08615655df55c07fc99d0ea
Jojo Zhang <jojozhang0327@gmail.com>
2020-04-20 00:05:38 -0400

    Update 20200419.js

--- 20200419.js
+++ 20200419.js
@@ -6,3 +6,26 @@ function count(deck) {
 function AlphabetSoup(str) {
 	return str.split("").sort().join("")
 }
+
+class BasicPlan {
+	static canStream = true;
+	static canDownload = true;
+	static numOfDevices = 1;
+	static hasSD = true;
+	static hasHD = false;
+	static hasUHD = false;
+	static price = '$8.99';
+}
+
+// Write the classes for StandardPlan and PremiumPlan here!
+class StandardPlan extends BasicPlan {
+		static hasHD = true;
+		static numOfDevices = 2;
+    static price = "$12.99"
+}
+
+class PremiumPlan extends StandardPlan {
+	  static hasUHD = true;
+		static numOfDevices = 4;
+		static price = "$15.99"
+}
