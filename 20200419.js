70b906aa6329e5e9657909d525c316a152aff53c
Jojo Zhang <jojozhang0327@gmail.com>
2020-04-19 23:51:26 -0400

    Update 20200419.js

--- 20200419.js
+++ 20200419.js
@@ -1,4 +1,8 @@
-Black Jack game
+// Black Jack game
 function count(deck) {
 	return deck.reduce((acc, curr) => acc + (curr<=6 ? 1: curr<=9? 0 : -1), 0)
+}
+
+function AlphabetSoup(str) {
+	return str.split("").sort().join("")
 }
